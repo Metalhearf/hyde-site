8aed4177bc3b7a5280a0b387ed32280014320bca
Metalhearf <6446231+Metalhearf@users.noreply.github.com>
2025-03-03 01:19:36 +0000

    Fix root URL redirection to default locale (/en) instead of just 404ing

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import {defineConfig} from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    '/': '/en', // Prevents 404's on root domain.
+  },
   integrations: [
     starlight({
       title: 'The HyDE project',
